824579276c48a51fe631ecf926a100b00e361b45
Bu5hm4nn <bu5hm4nn@users.noreply.github.com>
2026-04-13 15:16:54 +0200

    chore: stop tracking dist artifacts

--- dist/bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import {
-  src_default
-} from "./chunk-VR7GEZRX.js";
-
-// src/bin.ts
-src_default.start({
-  transportType: "stdio"
-});
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import {
-  src_default
-} from "./chunk-VR7GEZRX.js";
-export {
-  src_default as default
-};
