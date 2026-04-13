00196cc6cc3d0277b1fa2f0e795c3448fd7697b1
bu5hm4nn <bu5hm4nn@users.noreply.github.com>
2026-04-13 16:13:05 +0200

    chore: stop tracking dist artifacts (#5)
    * chore: refresh build output and lockfile
    
    * chore: stop tracking dist artifacts
    
    * chore: keep lockfile unchanged in dist cleanup PR

--- dist/bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import {
-  src_default
-} from "./chunk-6QEN7QWE.js";
-
-// src/bin.ts
-src_default.start({
-  transportType: "stdio"
-});
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import {
-  src_default
-} from "./chunk-6QEN7QWE.js";
-export {
-  src_default as default
-};
